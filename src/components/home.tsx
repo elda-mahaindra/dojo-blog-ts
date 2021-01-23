46b3ff078c17e67beb2e45623edc5ce197ef248e
elda-mahaindra <faith030@gmail.com>
2021-01-23 18:21:27 +0700

    click events

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,10 +2,26 @@
 import { FunctionComponent } from "react";
 
 // ---------------------------------------------- the component
-const Home: FunctionComponent = () => (
-  <div className="home">
-    <h2>Homepage</h2>
-  </div>
-);
+const Home: FunctionComponent = () => {
+  // ---------------------------------------------- the component
+  const handleClick = () => {
+    console.log("hello, ninjas!");
+  };
+
+  const handleClickAgain = (name: string) => {
+    console.log(`hello, ${name}`);
+  };
+
+  // ---------------------------------------------- content
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+
+      <button onClick={handleClick}>Click me</button>
+
+      <button onClick={() => handleClickAgain("mario")}>Click me again</button>
+    </div>
+  );
+};
 
 export default Home;
