b51ef97eec80ea5fb8121a6a3f7d0630a2fb753c
elda-mahaindra <faith030@gmail.com>
2021-01-23 23:14:26 +0700

    handling fetch errors

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -10,15 +10,27 @@ const Home: FunctionComponent = () => {
   // ---------------------------------------------- local state
   const [blogs, setBlogs] = useState<IBlog[] | null>(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   // ---------------------------------------------- effects
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
-        .then((res) => res.json())
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data from that resource.");
+          }
+
+          return res.json();
+        })
         .then((data) => {
           setBlogs(data as IBlog[]);
           setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
@@ -26,6 +38,8 @@ const Home: FunctionComponent = () => {
   // ---------------------------------------------- content
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+
       {isPending && <div>Loading ...</div>}
 
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
