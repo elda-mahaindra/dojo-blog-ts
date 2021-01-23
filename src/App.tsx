694270ae76e1b3d6ca00354137186bfc1660cc2a
elda-mahaindra <faith030@gmail.com>
2021-01-23 18:13:42 +0700

    adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import { FunctionComponent } from "react";
 import Home from "./components/home";
 import Navbar from "./components/navbar";
 
-// ---------------------------------------------- styles import
-import "./App.css";
-
 // ---------------------------------------------- the component
 const App: FunctionComponent = () => (
   <div className="App">
