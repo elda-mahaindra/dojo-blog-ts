44b16bd6ac787239a330c085bd0ffefd0d58c341
elda-mahaindra <faith030@gmail.com>
2021-01-23 17:27:53 +0700

    creating a react application

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+// ---------------------------------------------- modules import
+import React, { FunctionComponent } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// ---------------------------------------------- the component
+const App: FunctionComponent = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+// ---------------------------------------------- modules import
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+// ---------------------------------------------- styles import
+import "./index.css";
+
+// ---------------------------------------------- app render
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
