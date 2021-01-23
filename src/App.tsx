8a63e29db0b1367b9b7c5548375b7f8d4915ca2b
elda-mahaindra <faith030@gmail.com>
2021-01-23 17:43:28 +0700

    dynamic values in templates

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ const App: FunctionComponent = () => {
   const title = `Welcome to the new blog`;
   const likes = 50;
 
+  const link = "http://www.google.com";
+
   // ---------------------------------------------- content
   return (
     <div className="App">
@@ -16,6 +18,16 @@ const App: FunctionComponent = () => {
         <h1>{title}</h1>
 
         <p>Liked {likes} times</p>
+
+        <p>{10}</p>
+
+        <p>{"hello, ninjas!"}</p>
+
+        <p>{[1, 2, 3, 4, 5]}</p>
+
+        <p>{Math.random() * 10}</p>
+
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
