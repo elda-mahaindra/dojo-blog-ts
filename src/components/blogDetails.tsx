fcf501093afaa40cc4861bf8d937ea2e09fd42c5
elda-mahaindra <faith030@gmail.com>
2021-01-25 23:59:00 +0700

    deleting blogs

--- src/components/blogDetails.tsx
+++ src/components/blogDetails.tsx
@@ -1,20 +1,30 @@
 // ---------------------------------------------- modules import
 import { FunctionComponent } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 import { useFetch } from "./hooks";
 import { proxy } from "../constants/proxy";
 import { IBlog } from "../models/blog";
 
+import * as ROUTES from "../constants/routes";
+
 // ---------------------------------------------- the component
 const BlogDetails: FunctionComponent = () => {
   // ---------------------------------------------- router state
   const { blog_id } = useParams<{ blog_id: string }>();
+  let history = useHistory();
 
   // ---------------------------------------------- local state
   const { data: blog, isPending, error } = useFetch<IBlog>(
     `${proxy}/blogs/${blog_id}`
   );
+
+  // ---------------------------------------------- handlers
+  const handleClick = () => {
+    fetch(`${proxy}/blogs/${blog_id}`, { method: "DELETE" }).then(() => {
+      history.push(ROUTES.LANDING);
+    });
+  };
 
   // ---------------------------------------------- content
   return (
@@ -30,6 +40,8 @@ const BlogDetails: FunctionComponent = () => {
           <p>Written by {blog.author}</p>
 
           <div>{blog.body}</div>
+
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
