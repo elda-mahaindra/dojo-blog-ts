2195ea16cce4f5da7805635c021deeda1156564e
elda-mahaindra <faith030@gmail.com>
2021-01-25 23:34:53 +0700

    controlled inputs

--- src/components/create.tsx
+++ src/components/create.tsx
@@ -1,11 +1,48 @@
 // ---------------------------------------------- modules import
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 
 // ---------------------------------------------- the component
-const Create: FunctionComponent = () => (
-  <div className="create">
-    <h2>Add a new Blog</h2>
-  </div>
-);
+const Create: FunctionComponent = () => {
+  // ---------------------------------------------- local state
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
+  // ---------------------------------------------- content
+  return (
+    <div className="create">
+      <h2>Add a new Blog</h2>
+
+      <form>
+        <label>Blog title:</label>
+
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog body:</label>
+
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+
+        <label>Blog author:</label>
+
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+
+          <option value="yoshi">yoshi</option>
+        </select>
+
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
+};
 
 export default Create;
